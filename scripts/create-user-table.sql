237a75f89271af62e0a9c75dd2cf5b59f95dc0be
Jose Quinteiro <jq@quinteiro.org>
2018-11-10 13:54:30 -0800

    Add table for user authentication

---
+++ scripts/create-user-table.sql
@@ -0,0 +1,13 @@
+﻿DROP TYPE IF EXISTS usertype;
+
+CREATE TYPE usertype AS ENUM ('admin', 'user');
+
+DROP TABLE IF EXISTS "users";
+
+CREATE TABLE "users" (
+  "id" bigserial PRIMARY KEY,
+  "username" text CHECK(char_length("username")<=100) UNIQUE,
+  "password" text CHECK(char_length("password")<=100) UNIQUE,
+  "type" usertype
+);
+
