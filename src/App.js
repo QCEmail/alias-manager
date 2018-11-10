e33fc2b2609284c491866a6379b80c5fd3537f12
Heath Chiavettone <github@chiavettone.com>
2018-11-10 14:24:21 -0800

    Added the login page - Added reactstrap and react-router - Added `LoginForm` - Made the App have fake authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 
+import LoginForm from './LoginForm';
 import './App.css';
-import { Alert } from 'reactstrap';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+    };
+  }
+
+  authenticateUser = (username, password) => {
+    if (!username && !password) {
+      return Promise.reject('Enter a username and password');
+    }
+    if (username === 'duh') {
+      return Promise.reject('The username and password you provided is not valid');
+    }
+    this.setState({ authenticated: true });
+    return Promise.resolve();
+  };
+
   render() {
+    const { authenticated } = this.state;
+
+    if (!authenticated) {
+      return (
+        <LoginForm authenticateUser={this.authenticateUser}/>
+      );
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <Alert color="primary">
-            Test
-          </Alert>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Stuff
         </header>
       </div>
     );
